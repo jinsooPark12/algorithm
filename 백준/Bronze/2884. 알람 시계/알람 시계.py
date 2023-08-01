b86556d1f8e5ade894cc977e8c9d7452c8b2b1e8
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2023-08-01 23:47:28 +0900

    [Bronze III] Title: 알람 시계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,10 +1,12 @@
 H, M = map(int, input().split())
 
-ans_M = M - 45
-
-if ans_M >= 0 :
-    print(f"{H} {ans_M}")
-elif ans_M < 0 and H != 0 :
-    print(f"{H - 1} {60 + ans_M}")
+if M < 45:
+    M += 15
+    H -= 1
 else:
-    print(f"23 {60 + ans_M}")
+    M -= 45
+
+if H == -1:
+    H = 23
+
+print(f"{H} {M}")
