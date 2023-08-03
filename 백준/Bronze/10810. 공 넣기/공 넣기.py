3aeb3952f0d2ee55ad4a4eac278665a215dc688f
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2023-08-03 15:29:40 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -3,8 +3,7 @@ basket = [0 for x in range(N)]
 
 for _ in range(M):
     i, j, k = map(int, input().split())
-    for l in range(N):
-        if l >= i - 1 and l <= j -1:
-            basket[l] = k
+    for l in range(i-1, j):
+        basket[l] = k
 
 print(*basket)
