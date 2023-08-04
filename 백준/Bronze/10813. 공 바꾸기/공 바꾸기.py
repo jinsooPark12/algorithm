01b7deed9003fcaa47ae7d02c201158802b66448
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2023-08-04 16:48:39 +0900

    [Bronze II] Title: 공 바꾸기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+basket = [i for i in range(1, N + 1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    ret = basket[a - 1]
+    basket[a - 1] = basket[b - 1]
+    basket[b - 1] = ret
+
+print(*basket)
